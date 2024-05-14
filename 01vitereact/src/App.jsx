53471d387c59c27208fe4558a3b9aeb55aced5d2
OmkarShinde09 <omkarshine07091969@gmail.com>
2024-05-14 20:59:52 +0530

    First commit. Learned how to build react project in vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Component";
+
+function App() {
+  return (
+    <>
+      <Chai/>
+      <h1>This is react project using vite | Omkar</h1>
+    </>//This is called a fragment. As React allows us to send only one element, we could have sent it with the div tag but it was later changed to a fragment.
+  );
+}
+
+export default App
---
+++ 01vitereact/src/Component.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>This is a component whose name is Chai!!</h3>
+    )
+}//Use proper conventions while naming the component. The first letter should be a capital letter. The files should be in the jsx format for vite.
+
+export default Chai
