81b5ac902cec19fc64f9dd330d6f50a91b539afc
OmkarShinde09 <omkarshine07091969@gmail.com>
2024-05-15 13:25:45 +0530

    Learned about hooks and created a small project of counter manipulation

---
+++ 03Counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(15)//useState is a hook that helps us to update the UI of the page by changing the counter value.
+
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
